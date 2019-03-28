ad5ca9036ccd379a0a6e247e8dd6515945a4064d
Paul Alcabasa <paulalcabasa@gmail.com>
2019-03-28 17:37:54 +0800

    2nd day of tutorials

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,23 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment';
 import { Form, HasError, AlertError} from 'vform';
+
+
+import SweetAlert2 from 'sweetalert2'
+window.swal = SweetAlert2;
+
+const toast = swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.Toast = toast;
+
+import VueProgressBar from 'vue-progressbar';
 window.Form = Form;
 Vue.component(HasError.name, HasError);
 Vue.component(AlertError.name, AlertError);
@@ -16,6 +32,7 @@ import VueRouter from 'vue-router';
 import Dashboard from './components/Dashboard.vue';
 import Profile from './components/Profile.vue';
 import Users from './components/Users.vue';
+
 
 Vue.use(VueRouter);
 
@@ -30,6 +47,22 @@ const router = new VueRouter({
 	base: 'laravue',
 	routes
 });
+
+Vue.filter('upText', function(text){
+	return text.charAt(0).toUpperCase() + text.slice(1);
+});
+
+Vue.filter('myDate', function(created){
+	return moment(created).format('MMMM Do YYYY');
+});
+
+Vue.use(VueProgressBar, {
+	color : 'rgb(143, 255, 199)',
+	failedColor : 'red',
+	height : '2px'
+});
+
+window.Fire = new Vue();
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -7,37 +7,42 @@
                 <h3 class="card-title">Users Table</h3>
 
                 <div class="card-tools">
-                  <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add New <i class="fas fa-user-plus faw"></i></button>
+                  <button class="btn btn-success" @click="newModal()">Add New <i class="fas fa-user-plus faw"></i></button>
                 </div>
               </div>
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">
                 <table class="table table-hover">
-                  <tbody>
-                    <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Type</th>
-                    <th>Modify</th>
-                  </tr>
-                  <tr>
-                    <td>183</td>
-                    <td>John Doe</td>
-                    <td>11-7-2014</td>
-                    <td>johndoe@gmail.com</td>
-                    <td>
-                        <a href="">
-                            <i class="fa fa-edit blue"></i>
-                        </a>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Type</th>
+                            <th>Registered at</th>
+                            <th>Modify</th>
+                        </tr>
+                    </thead>
 
-                        <a href="">
-                            <i class="fa fa-trash red"></i>
-                        </a>
-                    </td>
-                  </tr>
-                  
-                </tbody></table>
+                    <tbody>
+                        <tr v-for="user in users" :key="user.id">
+                            <td>{{ user.id }}</td>
+                            <td>{{ user.name }}</td>
+                            <td>{{ user.email }}</td>
+                            <td>{{ user.type | upText }}</td>
+                            <td>{{ user.created_at | myDate }}</td>
+                            <td>
+                            <a href="" @click.prevent="editModal(user)">
+                                <i class="fa fa-edit blue"></i>
+                            </a>
+
+                            <a href="" @click.prevent="deleteUser(user.id)">
+                                <i class="fa fa-trash red"></i>
+                            </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
               </div>
               <!-- /.card-body -->
             </div>
@@ -50,11 +55,13 @@
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add New User</h5>
+                <h5 class="modal-title" id="exampleModalLabel">{{ editMode ? 'Update User\'s Info' : 'Add New User' }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
+              <form @submit.prevent="editMode ? updateUser() : createUser()">
+
               <div class="modal-body">
 
                 <div class="form-group">
@@ -62,10 +69,11 @@
                         class="form-control" :class="{ 'is-invalid': form.errors.has('name') }" placeholder="Name">
                     <has-error :form="form" field="name"></has-error>
                 </div>
-
+ 
                 <div class="form-group">
                     <input v-model="form.email" type="email" name="email"
-                        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" placeholder="Name">
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" 
+                        placeholder="Email">
                     <has-error :form="form" field="email"></has-error>
                 </div>
 
@@ -74,7 +82,7 @@
                         class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }" 
                         placeholder="Short bio for user (Optional)">
                     </textarea>
-                    <has-error :form="form" field="email"></has-error>
+                    <has-error :form="form" field="bio"></has-error>
                 </div>
 
                  <div class="form-group">
@@ -87,12 +95,22 @@
                    <has-error :form="form" field="type"></has-error>
                 </div>
 
-
+                <div class="form-group">
+                    <input v-model="form.password" type="password" name="password" id="password"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }" 
+                        placeholder="Password"
+                    >
+                    <has-error :form="form" field="password"></has-error>
+                </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Create</button>
+                <button v-show="!editMode" type="submit" class="btn btn-primary">Create</button>
+                <button v-show="editMode" type="submit" class="btn btn-success">Update</button>
               </div>
+
+              </form>
+
             </div>
           </div>
         </div>
@@ -105,7 +123,10 @@
     export default {
         data() {
             return {
+                editMode : false,
+                users : {},
                 form: new Form({
+                    id : '',
                     name : '',
                     email : '',
                     password : '',
@@ -115,8 +136,93 @@
                 })
             }
         },
+        methods: {
+            updateUser(){
+               this.$Progress.start();
+               this.form.put('api/user/' + this.form.id)
+               .then( () => {
+                    $('#addNew').modal('hide');
+                    swal.fire(
+                        'Updated!',
+                        'User has been updated.',
+                        'success'
+                    );
+                    Fire.$emit('AfterCreate');
+                    this.$Progress.finish();
+               })
+               .catch( () => {
+                    this.$Progress.fail();
+               });
+            },
+            editModal(user){
+                this.editMode = true;
+                this.form.clear();
+                this.form.fill(user);
+                $('#addNew').modal('show');
+            },
+            newModal(){
+                this.editMode = false;
+                this.form.reset();
+                $('#addNew').modal('show');
+            },
+            loadUsers(){
+                axios.get("api/user").then(({data}) => (this.users = data.data));
+            },
+            createUser(){
+                this.$Progress.start();
+                this.form
+                .post('api/user')
+                .then( () => {
+                    Fire.$emit('AfterCreate');
+                    $('#addNew').modal('hide');
+                    Toast.fire({
+                        type: 'success',
+                        title: 'User Created in successfully'
+                    });
+                    this.$Progress.finish(); 
+                })
+                .catch( () => {
+                    this.Progress.fail()
+                });  
+            },
+            deleteUser(id){
+                swal.fire({
+                    title : 'Are you sure?',
+                    text : "You won't be able to revert this!",
+                    type : "warning",
+                    showCancelButton : true,
+                    confirmationButtonColor : '#3085d6',
+                    cancelButtonColor : '#d33',
+                    confirmButtonText : "Yes, delete id!"
+                }).then( (result) => {
+                    if(result.value){
+                        this.form.delete('api/user/' + id).then( () => {
+                            Fire.$emit('AfterCreate');
+                            swal.fire(
+                                'Deleted!',
+                                'User has been deleted.',
+                                'success'
+                            )
+                        }).catch( () => {
+                             swal.fire(
+                                'Failed!',
+                                'There was something wrong.',
+                                'warning'
+                            )
+                        }); 
+                    }
+                });
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        created(){
+            this.loadUsers();
+            Fire.$on('AfterCreate', () => {
+                this.loadUsers();
+            });
+            //setInterval( () => this.loadUsers(), 3000);
         }
     }
 </script>
